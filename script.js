0c557a6a04755e2aff62c040ef04a0054a34b17d
Asya-Mikhailova <54442754+Asya-Mikhailova@users.noreply.github.com>
2020-03-11 12:11:51 +0100

    feat: add js for portfolio tabs, change  header position to fixed

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const MENU = document.getElementById('menu');
+const PORTFOLIO_BUTTONS = document.querySelector('.portfolio-buttons');
+const PICTURES = document.querySelector('.layout-4-columns');
 
 MENU.addEventListener('click', (event)=>{
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
+});
+
+PORTFOLIO_BUTTONS.addEventListener('click', (event)=> {
+    PORTFOLIO_BUTTONS.querySelectorAll('button').forEach(el => el.classList.remove('white'));
+    event.target.classList.add('white');
+});
+
+PICTURES.addEventListener('click', (event)=>{
+    PICTURES.querySelectorAll('img').forEach(el=>el.classList.remove('border'));
+    event.target.classList.add('border');
 });
